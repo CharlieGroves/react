1ec5a962d8e989647bcc14a61965b1ea8e3f7af9
CharlieGroves <62213643+CharlieGroves@users.noreply.github.com>
2020-07-18 19:00:32 +0100

    nested-components

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './Footer'
+
+/* Use Capital Camel Case for this
+This needs to return just one element 
+so wrap in an empty element <>Like this</> 
+if you are trying to return more than one elements */
+
+function App() {
+    return (
+        <>
+            <Footer />
+        </>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-/* When importing a component, React assumes that you are importing a js file
-so you do not have to put .js at the end of './MyInfo' as .js is the default extension */
+import App from './components/App'
 
-import MyInfo from './components/MyInfo'
-
-// JSX
-ReactDOM.render(<MyInfo />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
